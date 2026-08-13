f44674a2ada9d4757da4b352790d9d2879871322
lelqwq <3406892692@qq.com>
2026-08-13 20:46:25 +0800

    feat(covers): 回移 1.0.0 覆盖板优化，适配 GTNH 2.8.4
    - 基类 CoverLegacyData -> Cover，配置随 NBT 持久化，
      修复区块重载/重进存档后覆盖板失联
    - 接入 4 参 onCoverRightClick，右键可查看配置
    - instanceof 模式匹配改写为经典写法，保持 jabel 工具链
    - 版本号 0.2.0 -> 0.2.1
    
    Refs: 5fbe83e, 9dbbe61

--- src/main/java/com/miaokatze/gtswn/common/covers/GTswn_Cover_EnergyWireless.java
+++ src/main/java/com/miaokatze/gtswn/common/covers/GTswn_Cover_EnergyWireless.java
@@ -1,4 +1,3 @@
-
 package com.miaokatze.gtswn.common.covers;
 
 import static gregtech.common.misc.WirelessNetworkManager.addEUToGlobalEnergyMap;
@@ -6,19 +5,27 @@ import static gregtech.common.misc.WirelessNetworkManager.addEUToGlobalEnergyMap
 import java.util.UUID;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ChatComponentText;
+
+import com.google.common.io.ByteArrayDataInput;
 
 import gregtech.api.covers.CoverContext;
 import gregtech.api.interfaces.tileentity.ICoverable;
 import gregtech.api.metatileentity.BaseMetaTileEntity;
-import gregtech.common.covers.CoverLegacyData;
+import gregtech.common.covers.Cover;
+import io.netty.buffer.ByteBuf;
 
 /**
  * 链路锚点（能源模式）
  * <p>
  * 从无线网络获取能量给机器
+ * <p>
+ * 2.8.4 分支：基于 1.0.0 的 Cover 基类重写（NBT/网络持久化 + 右键配置显示），
+ * instanceof 模式匹配改写为经典写法以兼容 jabel 工具链。
  */
-public class GTswn_Cover_EnergyWireless extends CoverLegacyData {
+public class GTswn_Cover_EnergyWireless extends Cover {
 
     private int voltage = 0;
     private int amperage = 0;
@@ -27,7 +34,48 @@ public class GTswn_Cover_EnergyWireless extends CoverLegacyData {
     private boolean configured = false;
 
     public GTswn_Cover_EnergyWireless(CoverContext context) {
-        super(context);
+        super(context, null);
+    }
+
+    @Override
+    protected void readDataFromNbt(NBTBase nbt) {
+        if (nbt instanceof NBTTagCompound) {
+            NBTTagCompound tag = (NBTTagCompound) nbt;
+            if (tag.hasKey("voltage")) this.voltage = tag.getInteger("voltage");
+            if (tag.hasKey("amperage")) this.amperage = tag.getInteger("amperage");
+            if (tag.hasKey("intervalTicks")) this.intervalTicks = tag.getInteger("intervalTicks");
+            if (tag.hasKey("singleTransferEnergy")) this.singleTransferEnergy = tag.getLong("singleTransferEnergy");
+            if (tag.hasKey("configured")) this.configured = tag.getBoolean("configured");
+        }
+    }
+
+    @Override
+    protected void readDataFromPacket(ByteArrayDataInput byteData) {
+        voltage = byteData.readInt();
+        amperage = byteData.readInt();
+        intervalTicks = byteData.readInt();
+        singleTransferEnergy = byteData.readLong();
+        configured = byteData.readBoolean();
+    }
+
+    @Override
+    protected NBTBase saveDataToNbt() {
+        NBTTagCompound tag = new NBTTagCompound();
+        tag.setInteger("voltage", voltage);
+        tag.setInteger("amperage", amperage);
+        tag.setInteger("intervalTicks", intervalTicks);
+        tag.setLong("singleTransferEnergy", singleTransferEnergy);
+        tag.setBoolean("configured", configured);
+        return tag;
+    }
+
+    @Override
+    protected void writeDataToByteBuf(ByteBuf byteBuf) {
+        byteBuf.writeInt(voltage);
+        byteBuf.writeInt(amperage);
+        byteBuf.writeInt(intervalTicks);
+        byteBuf.writeLong(singleTransferEnergy);
+        byteBuf.writeBoolean(configured);
     }
 
     @Override
@@ -49,15 +97,14 @@ public class GTswn_Cover_EnergyWireless extends CoverLegacyData {
     public void doCoverThings(byte aInputRedstone, long aTimer) {
         if (!this.configured || this.singleTransferEnergy <= 0 || this.intervalTicks <= 0) return;
 
-        if (coverData == 0 || aTimer % this.intervalTicks == 0) {
+        if (aTimer % this.intervalTicks == 0) {
             tryFetchingEnergy();
         }
-        coverData = 1;
     }
 
     private static UUID getOwner(Object te) {
-        if (te instanceof BaseMetaTileEntity igte) {
-            return igte.getOwnerUuid();
+        if (te instanceof BaseMetaTileEntity) {
+            return ((BaseMetaTileEntity) te).getOwnerUuid();
         } else {
             return null;
         }
@@ -65,7 +112,8 @@ public class GTswn_Cover_EnergyWireless extends CoverLegacyData {
 
     private void tryFetchingEnergy() {
         ICoverable tileEntity = coveredTile.get();
-        if (tileEntity instanceof BaseMetaTileEntity bmte) {
+        if (tileEntity instanceof BaseMetaTileEntity) {
+            BaseMetaTileEntity bmte = (BaseMetaTileEntity) tileEntity;
             long currentEU = bmte.getStoredEUuncapped();
             long capacity = bmte.getEUCapacity();
             long neededEU = capacity - currentEU;
@@ -94,7 +142,8 @@ public class GTswn_Cover_EnergyWireless extends CoverLegacyData {
         return true;
     }
 
-    public boolean onCoverRightClick(EntityPlayer aPlayer) {
+    @Override
+    public boolean onCoverRightClick(EntityPlayer aPlayer, float aX, float aY, float aZ) {
         aPlayer.addChatMessage(
             new ChatComponentText(net.minecraft.util.StatCollector.translateToLocal("gtswn.chat.cover.energy_config")));
         if (this.configured) {
